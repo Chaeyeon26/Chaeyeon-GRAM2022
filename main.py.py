40b2cbebb4b847684586e6c42aa8f43e3498464b
Chaeyeon26 <wdslisa8@naver.com>
2024-03-14 12:41:18 +0900

    손채연 2022111603

---
+++ main.py.py
@@ -0,0 +1,8 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def main():
+...     stu_info = input("이름 학번 학과도: ")
+...     
+...     print(stu_info)
+...     
+... if __name__ == "__main__":
